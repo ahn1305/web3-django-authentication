392647f8f6af87a2450e93b072da3de4622901de
ahn1305 <ahnashwin1305@gmail.com>
2023-04-16 06:42:32 +0530

    wheel file made

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='web3-django-authentication',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['ethereum==2.3.2', 'rlp<=2.0.0', 'eth_utils>=1.0.3', 'Django>=2.0'],
+    entry_points={
+        'console_scripts': [
+            'web3-django-authentication=web3auth.views:main',
+        ],
+    },
+    author='Ashwin B',
+    author_email='ahnashwin1305@gmail.com',
+    description='Integrate metamask with django' 	 	,
+    url='https://github.com/ahn1305/web3-django-authentication',
+)
