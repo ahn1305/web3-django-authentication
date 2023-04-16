ed22320f234c0c264afc9a03c903412484cdffd7
ahn1305 <ahnashwin1305@gmail.com>
2023-04-16 06:22:07 +0530

    changes in setup

--- setup.py
+++ setup.py
@@ -10,18 +10,18 @@ except ImportError:
     from distutils.core import setup
 
 
-def get_version(*file_paths):
-    """Retrieves the version from web3auth/__init__.py"""
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
+# def get_version(*file_paths):
+#     """Retrieves the version from web3auth/__init__.py"""
+#     filename = os.path.join(os.path.dirname(__file__), *file_paths)
+#     version_file = open(filename).read()
+#     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+#                               version_file, re.M)
+#     if version_match:
+#         return version_match.group(1)
+#     raise RuntimeError('Unable to find version string.')
 
 
-version = get_version("web3auth", "__init__.py")
+# version = get_version("web3auth", "__init__.py")
 
 if sys.argv[-1] == 'publish':
     try:
